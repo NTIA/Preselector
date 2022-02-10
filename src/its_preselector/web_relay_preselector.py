43fbcdd7e9645de4c257c37faca3f44fcddb7cd4
dboulware <dboulware@ntia.doc.gov>
2022-02-10 14:03:10 -0700

    Added noise_diode_off to config. Removed set_source method and raise exception if you try to set a source that doesn't exist in config.

--- src/its_preselector/web_relay_preselector.py
+++ src/its_preselector/web_relay_preselector.py
@@ -4,18 +4,14 @@ import requests
 
 class WebRelayPreselector(Preselector):
 
-    def set_rf_path_on(self, i):
+    def set_rf_path(self, i):
         key = str(i)
         if key in self.config:
             switches = self.config[str(i)].split(',')
-            for i in range(len(switches)):
-                command = self.base_url + switches[i]
-                print(command)
-                requests.get(command)
-
-    def set_source(self, key):
-        switches = self.config[key].split(',')
-        for i in range(len(switches)):
-            command = self.config['WEB_RELAY']['base_url'] + switches[i]
-            print(command)
-            requests.get(command)
+            if self.base_url:
+                for i in range(len(switches)):
+                    command = self.base_url + switches[i]
+                    print(command)
+                    requests.get(command)
+        else:
+            raise Exception("RF path " + key + " configuration does not exist.")
